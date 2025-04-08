4381c780c85d6094609ca09a62609c3e3b0da86b
LANCECRUZ-STUDENT <g00428876@atu.ie>
2025-04-08 09:46:01 +0100

    Added test case for titles

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -14,14 +14,30 @@ public class PassengerTest {
 
     @Test
     void getTitleSuccess(){
+        newPass.setTitle("Mr");
         assertEquals("Mr", newPass.getTitle());
     }
 
     @Test
     void getTitleFailure(){
+        newPass.setTitle("abc");
         Exception ex = assertThrows(IllegalArgumentException.class, () -> {
             newPass.getTitle();});
         assertEquals("Must be Mr, Mrs or Ms", ex.getMessage());
+    }
+
+    @Test
+    void getFirstNameSuccess(){
+        newPass.setFirst_name("Mark");
+        assertEquals("Mark", newPass.getFirst_name());
+    }
+
+    @Test
+    void getFirstNameFailure(){
+        newPass.setFirst_name("ac");
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
+            newPass.getFirst_name();});
+        assertEquals("Can't be less then 3 characters", ex.getMessage());
     }
 
 }
